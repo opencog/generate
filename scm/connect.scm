139f8f536cb606d47b2dbf3953bf0d00fdbf2ecf
Linas Vepstas <linasvepstas@gmail.com>
2020-03-09 11:30:43 -0500

    Add the first query

--- scm/connect.scm
+++ scm/connect.scm
@@ -20,3 +20,54 @@
 		(Connector
 			(Concept "A")
 			(ConnectorDir "-"))))
+
+; ------------------------------------------------------
+;
+(define (get-connectable-sections CONNECTOR POL-L POL-R)
+"
+  Return a set of section-pairs that could be connected together.
+  CONNECTOR is the atom in the connectors to join together
+  POL-L and POL-R are the polarities to join together.
+"
+	(define getter
+		(Bind
+			(VariableList
+				(Variable "$latom") (Glob "$lfront") (Glob "$lback")
+				(Variable "$ratom") (Glob "$rfront") (Glob "$rback"))
+			(And
+				(Section
+					(Variable "$latom")
+					(ConnectorSeq
+						(Glob "$lfront")
+						(Connector CONNECTOR POL-L)
+						(Glob "$lback")))
+				(Section
+					(Variable "$ratom")
+					(ConnectorSeq
+						(Glob "$rfront")
+						(Connector CONNECTOR POL-R)
+						(Glob "$rback"))))
+
+			; Just repeat what was joined.
+			(List
+				(Section
+					(Variable "$latom")
+					(ConnectorSeq
+						(Glob "$lfront")
+						(Connector CONNECTOR POL-L)
+						(Glob "$lback")))
+				(Section
+					(Variable "$ratom")
+					(ConnectorSeq
+						(Glob "$rfront")
+						(Connector CONNECTOR POL-R)
+						(Glob "$rback"))))
+	))
+
+	(cog-execute! getter)
+)
+
+; ------------------------------------------------------
+;
+(get-connectable-sections
+	(Concept "A") (ConnectorDir "+") (ConnectorDir "-"))
