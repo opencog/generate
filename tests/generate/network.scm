b16f166cd547f1363f433f77f1e9b415a7c35b2d
Linas Vepstas <linasvepstas@gmail.com>
2020-04-10 08:49:01 -0500

    Start work on random  networks

---
+++ tests/generate/network.scm
@@ -0,0 +1,53 @@
+;
+; network.scm
+;
+; Basic test: Generate random network. Dictionary contains one word,
+; with arbitrary numbers of connectors, all of the same type.
+;
+(use-modules (srfi srfi-1))
+(use-modules (opencog) (opencog exec))
+
+(Section
+	(Concept "peep 1")
+	(ConnectorSeq
+		(Connector (Concept "E") (ConnectorDir "*"))))
+
+(Section
+	(Concept "peep 2")
+	(ConnectorSeq
+		(Connector (Concept "E") (ConnectorDir "*"))
+		(Connector (Concept "E") (ConnectorDir "*"))))
+
+(Section
+	(Concept "peep 3")
+	(ConnectorSeq
+		(Connector (Concept "E") (ConnectorDir "*"))
+		(Connector (Concept "E") (ConnectorDir "*"))
+		(Connector (Concept "E") (ConnectorDir "*"))))
+
+(Section
+	(Concept "peep 4")
+	(ConnectorSeq
+		(Connector (Concept "E") (ConnectorDir "*"))
+		(Connector (Concept "E") (ConnectorDir "*"))
+		(Connector (Concept "E") (ConnectorDir "*"))
+		(Connector (Concept "E") (ConnectorDir "*"))))
+
+(Section
+	(Concept "peep 5")
+	(ConnectorSeq
+		(Connector (Concept "E") (ConnectorDir "*"))
+		(Connector (Concept "E") (ConnectorDir "*"))
+		(Connector (Concept "E") (ConnectorDir "*"))
+		(Connector (Concept "E") (ConnectorDir "*"))
+		(Connector (Concept "E") (ConnectorDir "*"))))
+
+(Section
+	(Concept "peep 6")
+	(ConnectorSeq
+		(Connector (Concept "E") (ConnectorDir "*"))
+		(Connector (Concept "E") (ConnectorDir "*"))
+		(Connector (Concept "E") (ConnectorDir "*"))
+		(Connector (Concept "E") (ConnectorDir "*"))
+		(Connector (Concept "E") (ConnectorDir "*"))
+		(Connector (Concept "E") (ConnectorDir "*"))))
