f0ecff96291faa90ba5531bc24b7eed6e84b9828
Linas Vepstas <linasvepstas@gmail.com>
2020-04-13 12:45:55 -0500

    Continue work on graph export

--- opencog/scm/opencog/gml-export.scm
+++ opencog/scm/opencog/gml-export.scm
@@ -5,6 +5,29 @@
 ; See https://en.wikipedia.org/wiki/Graph_Modelling_Language
 ;
 
+(define (graph-to-nodes GRAPH)
+	; A list of just the points
+	(define pt-list
+		(map (lambda (sect) (cog-outgoing-atom sect 0))
+			(cog-outgoing-set GRAPH)))
+
+	(fold
+		(lambda (point str)
+			(string-concatenate (list
+				"\tnode [\n"
+				"\t\tid xxx\n"
+				"\t\tlabel \""
+				(cog-name point)
+				"\"\n"
+				"\t]\n"
+				str
+			)))
+		""
+		; A list of the unique points in the set.
+		(delete-dup-atoms pt-list)
+	)
+)
+
 (define-public (export-to-gml GRAPH-SET)
 "
    export-to-gml GRAPH-SET
@@ -13,6 +36,14 @@
 	to a UTF-8 encoded text string in GML - Graph Modeling Language
    format.
 "
-	(define graph-lst (cog-outgoing-set GRAPH-SET)
-
+	(fold
+		(lambda (grph str)
+			(string-concatenate (list
+				"graph [\n"
+				"\tlabel \"placeholder label\"\n"
+				(graph-to-nodes grph)
+				"]\n"
+				str)))
+		""
+		(cog-outgoing-set GRAPH-SET))
 )
